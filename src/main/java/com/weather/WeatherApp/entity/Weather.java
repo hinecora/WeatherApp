fff5f96c5568175a731d9325fa6caf218d72a30d
Sanal Mandzhiev <leonnspinks@gmail.com>
2021-12-14 09:37:49 +0300

    refactoring

--- src/main/java/com/weather/WeatherApp/entity/Weather.java
+++ src/main/java/com/weather/WeatherApp/entity/Weather.java
@@ -1,10 +1,16 @@
 package com.weather.WeatherApp.entity;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "weather_history")
 public class Weather {
@@ -19,4 +25,16 @@ public class Weather {
     @Column(name = "weather_value")
     private String value;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Weather weather = (Weather) o;
+        return id != null && Objects.equals(id, weather.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
